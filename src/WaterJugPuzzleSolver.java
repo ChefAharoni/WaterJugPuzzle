2d6622fcdbf00329b7e679560d43334099165762
Amit Aharoni <ChefAharoni@proton.me>
2025-07-19 13:44:35 -0400

    * Added comparable to custom StepTup class to compare states of jugs
    * Added capacity to StepTup class for the jugs, auto-set by the variables in the constructor. Changed values a and b to zero since we know C will always be full.
    
    * Added getters and setter for vars in StepTup, with safety check in setter (no "overflow" of water in jugs)
    
    * Added enum for the jugs that represents 0-2 for the jugs to attempt using it in the moving method.
    
    * Broke down the algorithm further, attempting with while loop instead of a for loop.
    
    * Move method is having some issues I'm trying to solve, will try to use the enum next.

--- src/WaterJugPuzzleSolver.java
+++ src/WaterJugPuzzleSolver.java
@@ -1,38 +1,109 @@
-import java.util.Arrays;
 import java.util.LinkedList;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class WaterJugPuzzleSolver
 {
-    private class StepTup
+    private class StepTup implements Comparable<StepTup>
     {
-        private int x, y, z;
+        private int a, b, c;
+        private final int A_CAP, B_CAP, C_CAP; // capacity for each cap
 
-        public StepTup(int x, int y, int z)
+        public StepTup(int a, int b, int c)
         {
-            this.x = x;
-            this.y = y;
-            this.z = z;
+            this.a = 0;
+            this.b = 0;
+            this.c = c; // Jug C will always start out completely full
+
+            this.A_CAP = a;
+            this.B_CAP = b;
+            this.C_CAP = c;
+        }
+
+        public int getA()
+        {
+            return a;
+        }
+
+        public int getB()
+        {
+            return b;
+        }
+
+        public int getC()
+        {
+            return c;
+        }
+
+        public int getA_CAP()
+        {
+            return A_CAP;
+        }
+
+        public int getB_CAP()
+        {
+            return B_CAP;
+        }
+
+        public int getC_CAP()
+        {
+            return C_CAP;
+        }
+
+        public void setA(int a)
+        {
+            if (a <= this.A_CAP)
+                this.a = a;
+        }
+
+        public void setB(int b)
+        {
+            if (b <= this.B_CAP)
+                this.b = b;
+        }
+
+        public void setC(int c)
+        {
+            if (c <= this.C_CAP)
+                this.c = c;
+        }
+
+        @Override
+        public int compareTo(StepTup o)
+        {
+            if (this.a == o.a && this.b == o.b && this.c == o.c)
+                return 0;
+            else if (this.a < o.a || this.b < o.b || this.c < o.c)
+                return -1;
+            else
+                return 1;
         }
 
         @Override
         public String toString()
         {
-            return "(" + x + "," + y + "," + z + ")";
+            return "(" + a + "," + b + "," + c + ")";
         }
+
+        /* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+           ~~~~~~~~~~~~~~~~~~~~~~ END OF StepTup CLASS ~~~~~~~~~~~~~~~~~~~~~~
+           ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+         */
     }
 
     private int a, b, c; // Caps
     private int d, e, f; // Goals
 
-//    private final int[][] vars; // Variations
+    private enum Jug {A, B, C};
+
+    StepTup finalState;
+
+    //    private final int[][] vars; // Variations
     private final LinkedList<StepTup>[][] vars; // Variations
 
     // TODO: change from Integer type to a different type
 //    private LinkedList<StepTup> step;
 
-    @SuppressWarnings("unchecked")
     public WaterJugPuzzleSolver(int a, int b, int c, int d, int e, int f)
     {
         super(); // TODO: not sure if this is correct
@@ -57,19 +128,67 @@ public class WaterJugPuzzleSolver
 //        vars = new int[b+1][a+1];
         // TODO: fix this horrible array init
         vars = new LinkedList[4][6];
+
+        // set the final state from the params
+        finalState = new StepTup(d, e, f);
     }
 
-    private void resetVarsArr()
-    { // TODO: fix these loops' constrains (hard-coded values)
-        for (int i = 0; i < 4; i++)
+//    private void resetVarsArr()
+//    { // TODO: fix these loops' constrains (hard-coded values)
+//        for (int i = 0; i < 4; i++)
+//        {
+//            for (int j = 0; j < 6; j++)
+//            {
+//                vars[i][j] = new LinkedList<>();
+//                vars[i][j].add(new StepTup(i, j, 0));
+//                vars[i][j].add(new StepTup(0, 0, 0));
+//            }
+//        }
+//    }
+
+    private void getWaysBFS()
+    {
+        StepTup currState = new StepTup(a, b, c); // not sure if needed
+
+//        while (a != d && b != e && c != f)
+        while (currState != finalState)
         {
-            for (int j = 0; j < 6; j++)
-            {
-                vars[i][j] = new LinkedList<>();
-                vars[i][j].add(new StepTup(i, j, 0));
-            }
+            // TODO: add stop if no solution
+
+            // 1) move c → a
+            moveWater(currState, Jug.C, Jug.A);
+            // 2) move b → a
+
+            // 3) move c → b
+
+            // 4) move a → b
+
+            // 5) move b → c
+
+            // 6) move a → c
+
+
         }
     }
+
+    /**
+     * e.g. `from` is c and `to` is a
+     * Ensures no more than capacity is moved
+     */
+    private StepTup moveWater(StepTup state, Jug from, Jug to)
+    {
+        StepTup newState = new StepTup(state.a, state.b, state.c); // copy of
+        switch(from)
+        {
+            case A:
+
+        }
+        newState.to = newState.from - (newState.from - newState.toCapacity);
+        newState.from = newState.from - newState.to;
+
+        return newState;
+    }
+
 
     // TODO: I'm a debugger, delete me
     private void printVarsArr()
@@ -79,7 +198,7 @@ public class WaterJugPuzzleSolver
         {
             for (int j = 0; j < 6; j++)
             {
-                System.out.print(vars[i][j].toString() + (j+1 == 6 ? "" : ", "));
+                System.out.print(vars[i][j].toString() + (j + 1 == 6 ? "" : ", "));
             }
 
             System.out.println();
@@ -90,12 +209,9 @@ public class WaterJugPuzzleSolver
     public void solve()
     {
         //
-        resetVarsArr();
-        printVarsArr();
+//        resetVarsArr();
+        printVarsArr(); // debugger
     }
-
-
-
 
 
     public static void main(String[] args)
