e6924a1b728f2e64341ffa502cfd86f9f6312add
Amit Aharoni <ChefAharoni@proton.me>
2025-07-19 22:04:27 -0400

    * Optimized imports
    * Changes StepTup to static (per IntelliJ rec)
    
    * Some comments deletion
    
    * Some code cleaning
    
    * Moved printSolution method to reasonable location in code
    
    * Added checkInput method to check the input for all errors and prints out a custom error for each

--- src/WaterJugPuzzleSolver.java
+++ src/WaterJugPuzzleSolver.java
@@ -1,12 +1,9 @@
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.List;
-import java.util.ArrayList;
-import java.util.Collections;
+import javax.swing.plaf.basic.BasicInternalFrameTitlePane;
+import java.util.*;
 
 public class WaterJugPuzzleSolver
 {
-    private class StepTup implements Comparable<StepTup>
+    private static class StepTup implements Comparable<StepTup>
     {
         private int a, b, c;
         private final int A_CAP, B_CAP, C_CAP; // capacity for each cap
@@ -78,24 +75,6 @@ public class WaterJugPuzzleSolver
             return C_CAP;
         }
 
-        public void setA(int a)
-        {
-            if (a <= this.A_CAP)
-                this.a = a;
-        }
-
-        public void setB(int b)
-        {
-            if (b <= this.B_CAP)
-                this.b = b;
-        }
-
-        public void setC(int c)
-        {
-            if (c <= this.C_CAP)
-                this.c = c;
-        }
-
         @Override
         public int compareTo(StepTup o)
         {
@@ -119,22 +98,24 @@ public class WaterJugPuzzleSolver
          */
     }
 
-    private int a, b, c; // Caps
-    private int d, e, f; // Goals
+    private int a;
+    private int b;
+    private int c; // Caps
+
+    private int d;
+    private int e;
+    private int f;
 
     // Numeric representation of the water jugs
-    private enum Jug {A, B, C};
+    private enum Jug
+    {A, B, C}
+
+    ;
 
     // Pool of all the states to run
     private Queue<StepTup> taskPool;
-
     StepTup finalState;
-
-    //    private final int[][] vars; // Variations
     private final StepTup[][] vars; // Variations
-
-    // TODO: change from Integer type to a different type
-//    private LinkedList<StepTup> step;
 
     public WaterJugPuzzleSolver(int a, int b, int c, int d, int e, int f)
     {
@@ -147,7 +128,7 @@ public class WaterJugPuzzleSolver
         this.b = b;
         this.c = c;
 
-        // Goal
+        // Goals
         this.d = d;
         this.e = e;
         this.f = f;
@@ -155,10 +136,9 @@ public class WaterJugPuzzleSolver
         // Variations
         // 2D array of b+1 rows, & a+1 columns
         // +1 is needed because we need to use 0 as well (0 water in jug)
-        vars = new StepTup[a+1][b+1];
+        vars = new StepTup[a + 1][b + 1];
 
         // set the final state from the params
-        // calling the capacity of the initial jugs (though it shouldn't matter)
         StepTup tmpState = new StepTup(d, e, f, d, e, f);
         finalState = new StepTup(d, e, f, tmpState);
         this.taskPool = new LinkedList<>();
@@ -177,44 +157,6 @@ public class WaterJugPuzzleSolver
                 break;
             }
         }
-    }
-
-    private void printSolutionPath(StepTup goal) {
-        // build the path backwards
-        List<StepTup> path = new ArrayList<>();
-        for (StepTup cur = goal; cur != null; cur = cur.getParent())
-            path.add(cur);
-        Collections.reverse(path);
-
-        // accumulate into a single StringBuilder
-        StringBuilder out = new StringBuilder(path.size() * 30);
-        out.append("Initial state: ").append(path.getFirst()).append('\n');
-
-        for (int i = 1; i < path.size(); i++) {
-            StepTup from = path.get(i - 1), to = path.get(i);
-            int da = to.getA() - from.getA();
-            int db = to.getB() - from.getB();
-            int dc = to.getC() - from.getC();
-
-            String src = da < 0 ? "A" : db < 0 ? "B" : "C";
-            String dst = da > 0 ? "A" : db > 0 ? "B" : "C";
-            int moved = da != 0 ? Math.abs(da)
-                    : db != 0 ? Math.abs(db)
-                    : Math.abs(dc);
-
-            // simple concatenation, no format parsing
-            out.append("Pour ")
-                    .append(moved)
-                    .append(" gallons from ")
-                    .append(src)
-                    .append(" to ")
-                    .append(dst)
-                    .append(". ")
-                    .append(to)
-                    .append('\n');
-        }
-
-        System.out.print(out);
     }
 
     private void getWaysBFS(StepTup state)
@@ -274,18 +216,37 @@ public class WaterJugPuzzleSolver
 
         switch (from)
         {
-            case A: amountFrom = aAmt; break;
-            case B: amountFrom = bAmt; break;
-            case C: amountFrom = cAmt; break;
-            default: System.out.println("null"); return null; // error
+            case A:
+                amountFrom = aAmt;
+                break;
+            case B:
+                amountFrom = bAmt;
+                break;
+            case C:
+                amountFrom = cAmt;
+                break;
+            default:
+                System.out.println("null");
+                return null; // error
         }
 
         switch (to)
         {
-            case A: amountTo = aAmt; capacityTo = aCap; break;
-            case B: amountTo = bAmt; capacityTo = bCap; break;
-            case C: amountTo = cAmt; capacityTo = cCap; break;
-            default: System.out.println("null"); return null; // error
+            case A:
+                amountTo = aAmt;
+                capacityTo = aCap;
+                break;
+            case B:
+                amountTo = bAmt;
+                capacityTo = bCap;
+                break;
+            case C:
+                amountTo = cAmt;
+                capacityTo = cCap;
+                break;
+            default:
+                System.out.println("null");
+                return null; // error
         }
 
         // compute how much we can transfer
@@ -298,7 +259,7 @@ public class WaterJugPuzzleSolver
         // TODO: check transferable math:max of c out of the capacity
 
         amountFrom -= transferable;
-        amountTo   += transferable;
+        amountTo += transferable;
 
 //        System.out.println("transferable: " + transferable);
 //        System.out.println("amountFrom: " + amountFrom);
@@ -335,22 +296,44 @@ public class WaterJugPuzzleSolver
         return new StepTup(aAmt, bAmt, cAmt, state);
     }
 
-
-    // TODO: I'm a debugger, delete me
-    private void printVarsArr()
+    private void printSolutionPath(StepTup goal)
     {
-//        System.out.println(Arrays.deepToString(vars));
-        for (int i = 0; i < (a+1); i++)
-        {
-            for (int j = 0; j < (b+1); j++)
-            {
-                if (vars[i][j] == null) System.out.print("");
-                else
-                    System.out.print(vars[i][j].toString() + (j + 1 == (b+1) ? "" : ", "));
-            }
+        // build the path backwards
+        List<StepTup> path = new ArrayList<>();
+        for (StepTup cur = goal; cur != null; cur = cur.getParent())
+            path.add(cur);
+        Collections.reverse(path);
 
-            System.out.println();
+        // accumulate into a single StringBuilder
+        StringBuilder out = new StringBuilder(path.size() * 30);
+        out.append("Initial state: ").append(path.getFirst()).append('\n');
+
+        for (int i = 1; i < path.size(); i++)
+        {
+            StepTup from = path.get(i - 1), to = path.get(i);
+            int da = to.getA() - from.getA();
+            int db = to.getB() - from.getB();
+            int dc = to.getC() - from.getC();
+
+            String src = da < 0 ? "A" : db < 0 ? "B" : "C";
+            String dst = da > 0 ? "A" : db > 0 ? "B" : "C";
+            int moved = da != 0 ? Math.abs(da)
+                    : db != 0 ? Math.abs(db)
+                    : Math.abs(dc);
+
+            // simple concatenation, no format parsing
+            out.append("Pour ")
+                    .append(moved)
+                    .append(" gallons from ")
+                    .append(src)
+                    .append(" to ")
+                    .append(dst)
+                    .append(". ")
+                    .append(to)
+                    .append('\n');
         }
+
+        System.out.print(out);
     }
 
     public void solve()
@@ -359,29 +342,85 @@ public class WaterJugPuzzleSolver
         vars[a][b] = startState; // add init state to matrix
 
         getWaysBFS(startState);
-//        setDemoVals();
-//        getWaysBFS();
-//        resetVarsArr();
 
         advancePoolBFS();
 //        printVarsArr(); // debugger
     }
 
 
+    private static int[] checkInput(String[] args) {
+        // check arg count
+        if (args.length != 6) {
+            System.err.println(
+                    "Usage: java WaterJugPuzzleSolver " +
+                            "<cap A> <cap B> <cap C> <goal A> <goal B> <goal C>"
+            );
+            System.exit(1);
+        }
+
+        // check numeric and parse
+        int[] vals = new int[6];
+        for (int i = 0; i < 6; i++) {
+            String s = args[i];
+            // reject empty or non-digit strings
+            if (s.isEmpty() || !s.chars().allMatch(Character::isDigit)) {
+                String kind = (i < 3) ? "capacity" : "goal";
+                Jug jug = (i < 3)
+                        ? Jug.values()[i]
+                        : Jug.values()[i - 3];
+                System.err.printf(
+                        "Error: Invalid %s '%s' for jug %s.%n",
+                        kind, s, jug
+                );
+                System.exit(1);
+            }
+            // parse once
+            vals[i] = Integer.parseInt(s);
+        }
+
+        // assign to local names for clarity
+        int capA  = vals[0], capB  = vals[1], capC  = vals[2];
+        int goalA = vals[3], goalB = vals[4], goalC = vals[5];
+
+        // validate goal ≤ corresponding capacity
+        if (goalA > capA) {
+            System.err.println("Error: Goal cannot exceed capacity of jug A.");
+            System.exit(1);
+        }
+        if (goalB > capB) {
+            System.err.println("Error: Goal cannot exceed capacity of jug B.");
+            System.exit(1);
+        }
+        if (goalC > capC) {
+            System.err.println("Error: Goal cannot exceed capacity of jug C.");
+            System.exit(1);
+        }
+
+        // validate conservation: total goal water == capC
+        if (goalA + goalB + goalC != capC) {
+            System.err.println(
+                    "Error: Total gallons in goal state must be equal " +
+                            "to the capacity of jug C."
+            );
+            System.exit(1);
+        }
+
+        return vals;
+    }
+
     public static void main(String[] args)
     {
-        // Commented for dev, uncomment later
-//        if (args.length != 6)
-//        {
-//            System.err.println("Usage: java WaterJugPuzzleSolver " +
-//                    "<cap A> <cap B> <cap C> <goal A> <goal B> <goal C>");
-//            System.exit(1);
-//        }
 
-        WaterJugPuzzleSolver solver = new
-                WaterJugPuzzleSolver(3, 5, 8, 0, 4, 4);
+//      Manual input for debugging
+//      WaterJugPuzzleSolver solver = new WaterJugPuzzleSolver(3, 5, 8, 0, 4, 4);
 
-        solver.solve();
+        int[] v = checkInput(args);
+        new WaterJugPuzzleSolver(
+                v[0], v[1], v[2],
+                v[3], v[4], v[5]
+        ).solve();
+
+
 
         /* TODO: enter input checks:
         ------ Can be done in one function! ---------
@@ -401,4 +440,6 @@ public class WaterJugPuzzleSolver
 
         System.exit(0);
     }
+
+
 }
